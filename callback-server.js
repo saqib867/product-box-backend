fb14fdb9fb1871e327d2ed8e29e966a378aefd6a
saqib867 <saqibhussain9910@gmail.com>
2025-03-16 12:51:30 +0500

    backend task completed

--- callback-server.js
+++ callback-server.js
@@ -3,6 +3,7 @@
 
 // I have developed different services for each functionality. You need to run one service at one time by
 //  changing the " main " in the package.json file accoding to script tag in package.json
+// I can make just one server with different controller and routes, since the task insisted to use the one and only route given there
 
 
 import express from "express";
